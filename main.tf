6ad7223a0ee505b30178366b370dbe4a3ef15242
Sam-2025 <smunaff64@gmail.com>
2025-03-07 14:56:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "sample" {
   ami = "ami-0d682f26195e9ec0f"
   subnet_id = "subnet-07282fa9802d2992e"
   key_name = "samkey"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   tags = {
     Name = "SCM" 
   }
